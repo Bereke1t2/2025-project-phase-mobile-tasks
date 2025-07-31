e38c5772dd7d3547b65e2c696b788421e70e2832
bereke1t2 <bereketaschalew1212@gmail.com>
2025-07-31 13:49:34 +0000

    edited some part--

--- ecommerce_app/lib/futures/product/data/repositories/product_repository_impl.dart
+++ ecommerce_app/lib/futures/product/data/repositories/product_repository_impl.dart
@@ -1,66 +1,44 @@
 import 'package:dartz/dartz.dart';
+import 'package:test/test.dart';
 
 import '../../../../core/errors/failure.dart';
 // import '../../data/models/product_model.dart';
 import '../../domain/entities/product_entity.dart';
 import '../../domain/repositories/product_repository.dart';
+import '../data_sources/local/product_local_data_source.dart';
 import '../data_sources/remote/product_remote_data_sources.dart';
 import '../models/product_model.dart';
+import '../../../../core/platform/networkinfo.dart';
 
 class ProductRepositoryImpl implements ProductRepository {
 
   final ProductRemoteDataSources productRemoteDataSource;
+  final ProductLocalDataSources productLocalDataSource;
+  final NetworkInfo networkInfo;
 
-  ProductRepositoryImpl(this.productRemoteDataSource);
+  ProductRepositoryImpl(this.productRemoteDataSource, this.productLocalDataSource, this.networkInfo);
 
   @override
   Future<Either<Failure, List<ProductEntity>>> getAllProducts() async {
-    try {
-      final products = await productRemoteDataSource.fetchProducts();
-      return Right(products);
-    } catch (e) {
-      return const Left(ServerFailure());
-    }
+    throw UnimplementedError();
   }
 
   @override
   Future<Either<Failure, ProductEntity>> getProductById(String id) async {
-    try {
-      final product = await productRemoteDataSource.fetchProductById(id);
-      return Right(product);
-    } catch (e) {
-      return const Left(ServerFailure());
-    }
+    throw UnimplementedError();
   }
   @override
   Future<Either<Failure, void>> insertProduct(ProductEntity product) async {
-    try {
-      final productModel = ProductModel.fromEntity(product);
-      await productRemoteDataSource.addProduct(productModel);
-      return const Right(null);
-    } catch (e) {
-      return const Left(ServerFailure());
-    }
+    throw UnimplementedError();
   }
 
   @override
   Future<Either<Failure, void>> updateProduct(ProductEntity product) async {
-    try {
-      final productModel = ProductModel.fromEntity(product);
-      await productRemoteDataSource.updateProduct(productModel);
-      return const Right(null);
-    } catch (e) {
-      return const Left(ServerFailure());
-    }
+    throw UnimplementedError();
   }
 
   @override
   Future<Either<Failure, void>> deleteProduct(String id) async {
-    try {
-      await productRemoteDataSource.deleteProduct(id);
-      return const Right(null);
-    } catch (e) {
-      return const Left(ServerFailure());
-    }
+    throw UnimplementedError();
   }
 }
